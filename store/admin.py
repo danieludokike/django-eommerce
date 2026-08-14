c2ce84b871f19a089547fb4e2e50a46ce70145b8
= <danieludokike@gmail.com>
2026-08-14 22:17:30 +0100

    Included admin display

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import Category, Product, Order, OrderItem, ShippingAddress
 
-# Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name',)}
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'price', 'stock', 'available', 'created_at']
+    list_filter = ['available', 'created_at']
+    list_editable = ['price', 'stock', 'available']
+    prepopulated_fields = {'slug': ('name',)}
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    raw_id_fields = ['product']
+
+class ShippingAddressInline(admin.StackedInline):
+    model = ShippingAddress
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['id', 'full_name', 'email', 'paid', 'total_price', 'created_at']
+    list_filter = ['paid', 'created_at']
+    inlines = [OrderItemInline, ShippingAddressInline]
